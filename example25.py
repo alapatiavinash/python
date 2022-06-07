8ba7cd852bc55cd60ad17709c7434992de0a71a9
alapatiavinash <avinash2@pingalasoftware.com>
2022-06-07 18:13:59 +0530

    generators and file handling

---
+++ example25.py
@@ -0,0 +1,13 @@
+# Initialize the list
+my_list = [1, 3, 6, 10]
+
+a = (x**2 for x in my_list)
+print(next(a))
+
+print(next(a))
+
+print(next(a))
+
+print(next(a))
+
+next(a)
---
+++ example27.py
@@ -0,0 +1,4 @@
+file = open('file.txt', 'r')
+# This will print every line one by one in the file
+for each in file:
+    print (each)
